20dc7650adcaa7afbfdc96987635bed2228ee37a
Your Name <you@example.com>
2025-05-23 04:06:36 +0530

    day_7(greedy)

---
+++ VowelStartingUniquePatients.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class VowelStartingUniquePatients {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        LinkedHashSet<String> uniqueVowelNames = new LinkedHashSet<>();
+        String vowels = "aeiou";
+
+        for (int i = 0; i < n; i++) {
+            String name = sc.next();
+            if (vowels.indexOf(name.charAt(0)) != -1) {
+                uniqueVowelNames.add(name);
+            }
+        }
+
+        System.out.println(uniqueVowelNames.size());
+        for (String name : uniqueVowelNames) {
+            System.out.println(name);
+        }
+    }
+}
